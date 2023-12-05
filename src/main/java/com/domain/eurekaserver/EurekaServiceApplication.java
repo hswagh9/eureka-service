bbc2c0fb91ef2d792639026df1721e64fd74a3c1
harshal wagh <hswagh9@gmail.com>
2023-12-05 09:11:43 +0530

    INITIAL BRANCH SETUP

---
+++ src/main/java/com/domain/eurekaserver/EurekaServiceApplication.java
@@ -0,0 +1,23 @@
+package com.domain.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EurekaServiceApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+
+	}
+
+}
