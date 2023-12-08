b10e93fc506904bfc49bc21880b67e11dc6aab94
harshal wagh <hswagh9@gmail.com>
2023-12-08 12:19:51 +0530

    fixed eureka health checkup

--- src/main/java/com/domain/eurekaserver/EurekaServiceApplication.java
+++ src/main/java/com/domain/eurekaserver/EurekaServiceApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/**
+ * 
+ * @author hwagh
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServiceApplication extends SpringBootServletInitializer {
